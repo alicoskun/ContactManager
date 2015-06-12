59822c96f58e9c4b7feb3c40ffdc82866beeba52
Ali <Ali@Ali>
2015-06-12 20:25:32 +0300

    First commit

---
+++ app/src/main/java/contactmanager/ex/ali/contactmanager/MainActivity.java
@@ -0,0 +1,81 @@
+package contactmanager.ex.ali.contactmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
+
+
+public class MainActivity extends Activity {
+
+    EditText nameTxt, phoneTxt, emailTxt, addressTxt;
+    //test
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTxt = (EditText) findViewById(R.id.txtName);
+        phoneTxt = (EditText) findViewById(R.id.txtPhone);
+        emailTxt = (EditText) findViewById(R.id.txtEmail);
+        addressTxt = (EditText) findViewById(R.id.txtAddress);
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("creaator");
+        tabSpec.setContent(R.id.tabCreator);
+        tabSpec.setIndicator("Creator");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("list");
+        tabSpec.setContent(R.id.tabContactList);
+        tabSpec.setIndicator("List");
+        tabHost.addTab(tabSpec);
+
+        final Button addBtn = (Button) findViewById(R.id.btnAdd);
+
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addBtn.setEnabled(!nameTxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
